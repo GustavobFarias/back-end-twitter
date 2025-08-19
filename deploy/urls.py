e433f0c9bf39165ea1d63b3fe7637e9498daca6c
Gustavo <gustavoburgerf@gmail.com>
2025-08-18 23:06:04 -0300

    ajustando as rotas

--- deploy/urls.py
+++ deploy/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from myproject import views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- deploy/views.py
+++ deploy/views.py
@@ -1,15 +1,12 @@
 from django.http import HttpResponse
-from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
-
 import git
 
 @csrf_exempt
 def update(request):
-    secret_key = "MINHA_CHAVE_SECRETA"
-    if request.method == "POST" and request.headers.get("Authorization") == f"Token {secret_key}":
-        repo = git.Repo('/home/gfarias/twitter-login')
+    if request.method == "POST":
+        repo = git.Repo('/home/gfarias/twitter-login')  # caminho correto do seu repositório
         origin = repo.remotes.origin
         origin.pull()
         return HttpResponse("Updated code on PythonAnywhere")
-    return HttpResponse("Unauthorized", status=401)
+    return HttpResponse("Couldn't update the code on PythonAnywhere")
